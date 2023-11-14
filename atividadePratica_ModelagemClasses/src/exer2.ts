8ca9c0ba51ce5196d7dd2727f51e8084da7c398b
Lucas Santos <lucas22.ferreira.007@gmail.com>
2023-11-14 19:23:10 -0300

    feito o exer 2 modelagem de classes

--- atividadePratica_ModelagemClasses/src/exer2.ts
+++ atividadePratica_ModelagemClasses/src/exer2.ts
@@ -1,4 +1,27 @@
+class Ball {
+    color: string;
+    circumference: number;
+    material: string;
 
+    constructor(color: string, circumference: number, material: string){
+        this.color = color
+        this.circumference = circumference
+        this.material = material
+    }
+
+    changeColor(newColor: string): void{
+        this.color = newColor
+    }
+    
+    showColor(): void{
+        console.log(this.color)
+    }
+}
+
+const ball = new Ball("Azul", 1.35, "Couro")
+
+ball.changeColor("Vermelho")
+ball.showColor()
 
 // 2. Crie uma classe que modele uma bola:
 // a. Atributos
