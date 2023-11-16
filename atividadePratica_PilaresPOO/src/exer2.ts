ad5228e15ff8f823b59b890ab71f6b77ecf7e3f2
Lucas Santos <lucas22.ferreira.007@gmail.com>
2023-11-16 00:01:28 -0300

    feito o exer 2

--- atividadePratica_PilaresPOO/src/exer2.ts
+++ atividadePratica_PilaresPOO/src/exer2.ts
@@ -1,3 +1,43 @@
+class Immobile {
+  constructor(public address: string, public price: number) {}
+}
+
+class ImmobileNew extends Immobile {
+  constructor(addressNewImmobile: string, priceNewImmobile: number) {
+    super(addressNewImmobile, priceNewImmobile);
+  }
+
+  addPriceNewImmobile(newValue: number): string {
+    const newValueImmobile = (this.price += newValue);
+    return `Um imóvel novo possui um adicional de R$${newValue.toFixed(
+      2
+    )}, por isso o valor atual do imóvel passa a ser R$${newValueImmobile.toFixed(
+      2
+    )}.`;
+  }
+}
+
+class ImmobileOld extends Immobile {
+  constructor(addressOldImmobile: string, priceOldImmobile: number) {
+    super(addressOldImmobile, priceOldImmobile);
+  }
+
+  discountPriceOldImmobile(newValue: number): string {
+    const newValueImmobile = (this.price -= newValue);
+    return `Um imóvel velho possui um desconto de R$${newValue.toFixed(
+      2
+    )}, por isso o valor atual do imóvel passa a ser R$${newValueImmobile.toFixed(
+      2
+    )}.`;
+  }
+}
+
+const immobileNew = new ImmobileNew("São Paulo", 200000);
+console.log(immobileNew.addPriceNewImmobile(50000));
+
+const immobileOld = new ImmobileOld("Vitória", 150000);
+console.log(immobileOld.discountPriceOldImmobile(40000));
+
 // 2. Crie a classe Imovel, que possui um endereço e um preço.
 // a. crie uma classe Novo, que herda Imovel e possui um adicional
 // no preço. Crie métodos de acesso e impressão deste valor
