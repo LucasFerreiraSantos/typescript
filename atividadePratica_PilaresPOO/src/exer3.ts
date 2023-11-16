c92e2b1805f0bc2d9237ba9901de66e81f889571
Lucas Santos <lucas22.ferreira.007@gmail.com>
2023-11-16 00:36:07 -0300

    feito o exer3

--- atividadePratica_PilaresPOO/src/exer3.ts
+++ atividadePratica_PilaresPOO/src/exer3.ts
@@ -1,3 +1,85 @@
+abstract class Animal {
+  constructor(
+    protected specie: string,
+    protected gender: string,
+    protected family: string
+  ) {}
+
+  abstract emitSound(): object;
+}
+
+class Dog extends Animal {
+  constructor(
+    public called: string,
+    specie: string,
+    gender: string,
+    family: string
+  ) {
+    super(specie, gender, family);
+  }
+
+  emitSound(): object {
+    return {
+      called: this.called,
+      especie: this.specie,
+      genero: this.gender,
+      familia: this.family,
+      som: "Au Au...",
+    };
+  }
+}
+
+class Horse extends Animal {
+  constructor(
+    public called: string,
+    specie: string,
+    gender: string,
+    family: string
+  ) {
+    super(specie, gender, family);
+  }
+
+  emitSound(): object {
+    return {
+      called: this.called,
+      especie: this.specie,
+      genero: this.gender,
+      familia: this.family,
+      som: "Iiirrrrí, rilinchin...",
+    };
+  }
+}
+
+class Cat extends Animal {
+  constructor(
+    public called: string,
+    specie: string,
+    gender: string,
+    family: string
+  ) {
+    super(specie, gender, family);
+  }
+
+  emitSound(): object {
+    return {
+      called: this.called,
+      especie: this.specie,
+      genero: this.gender,
+      familia: this.family,
+      som: "Miau...",
+    };
+  }
+}
+
+const dog = new Dog("CACHORRO", "Canis lupus familiaris", "Canis", "Canídeos");
+console.log(dog.emitSound());
+
+const horse = new Horse("CAVALO", "Equus ferus caballus", "Equus", "Equinos");
+console.log(horse.emitSound());
+
+const cat = new Cat("GATO", "Felis silvestris catus", "Felis", "Felídeos");
+console.log(cat.emitSound());
+
 // // 3. Dado o seguinte diagrama:
 
 //               ANIMAL
