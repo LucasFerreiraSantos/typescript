2b057a842beec2ed2ed172b5ec5cffd904257355
Lucas Santos <lucas22.ferreira.007@gmail.com>
2023-11-15 22:39:24 -0300

    create repository activityPillarsPOO

---
+++ atividadePratica_PilaresPOO/src/exer3.ts
@@ -0,0 +1,9 @@
+// // 3. Dado o seguinte diagrama:
+
+//               ANIMAL
+
+//   CACHORRO    CAVALO    GATO
+
+// // Identifique os atributos e comportamentos que são comuns entre os
+// // 3 animais e encapsule-os na superclasse Animal. Crie pelo menos 1
+// // comportamento diferente para cada animal.
